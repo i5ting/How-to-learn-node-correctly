aaa0e282d879ab52ed1161ddd3ee0c7610a59421
狼叔 <langshu.ssl@alibaba-inc.com>
2018-03-23 14:11:49 +0800

    Update 2018-03-23T06:11:48.604Z

---
+++ toc_conf.js
@@ -0,0 +1,20 @@
+var jquery_ztree_toc_opts = {
+    debug:false,
+    is_auto_number:true,
+    documment_selector:'.markdown-body',
+    ztreeStyle: {
+        width:'326px',
+        overflow: 'auto',
+        position: 'fixed',
+        'z-index': 2147483647,
+        border: '0px none',
+        left: '0px',
+        top: '0px',
+				// 'overflow-x': 'hidden',
+				'height': $(window).height() + 'px'
+    }
+}
+var markdown_panel_style = {
+    'width':'70%',
+    'margin-left':'25%'
+};
